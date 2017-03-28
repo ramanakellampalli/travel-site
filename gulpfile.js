4935ce4ad048705dc34209eb42fec9d7e158a068
Ramana <ramanakellampalli@gmail.com>
2017-03-28 17:07:47 -0400

    for Gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp");
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
 
 gulp.task('default', function(){ 
 console.log("Hooray - you created a gulp task.");
@@ -7,7 +12,9 @@ console.log("Hooray - you created a gulp task.");
 });
 
 gulp.task('styles', function() {
-	console.log("Imagine Sass or postCSS tasks running here.");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+ 	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 
