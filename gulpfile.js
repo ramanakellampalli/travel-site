fb12a88f9ca5343dd4d80a9e686ec1662f45da44
Ramana <ramanakellampalli@gmail.com>
2017-03-28 18:21:52 -0400

    for project added external css files and imported them into main css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 
 gulp.task('default', function(){ 
@@ -13,7 +14,7 @@ console.log("Hooray - you created a gulp task.");
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
  	.pipe(gulp.dest('./app/temp/styles'));
 });
 
