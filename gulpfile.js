114bf92798ac242f1fc88a7e2f2a2b3528b5f3e3
Ramana <ramanakellampalli@gmail.com>
2017-03-28 10:16:25 -0400

    Added files to run gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var watch = require('gulp-watch');
+
+gulp.task('default', function(){ 
+console.log("Hooray - you created a gulp task.");
+
+});
+
+gulp.task('styles', function() {
+	console.log("Imagine Sass or postCSS tasks running here.");
+});
+
+
+gulp.task('html', function(){
+	console.log("Imagine something useful being done to your HTML here.");  
+});
+
+gulp.task('watch', function(){
+	watch('./app/index.html', function(){ 
+	gulp.start('html');
+	});
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+});
